20152d2e8fcb4fa40e5f2c03bc97a092fd70b59a
Giridhar Shanbhag <geeks2022@gmail.com>
2020-12-22 20:48:26 +0530

    Minor changes

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -14,6 +14,7 @@ export default class ColorPanel extends Component {
         color="black"
       >
         <Divider />
+
         <Button icon="add" size="small" color="blue"></Button>
       </Sidebar>
     )
